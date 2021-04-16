4da329f8d792ba8f8429b1978872140586d99787
Dseus4 <tsumakig@gmail.com>
2021-04-15 21:16:25 -0700

    Implemented factory into EnemyGenerator

--- EnemyGenerator.java
+++ EnemyGenerator.java
@@ -5,43 +5,40 @@ import java.io.File;
  * Generate enemies in the game from the provided txt file.
  */
 class EnemyGenerator{
-    java.util.ArrayList<Enemy> enemyList = new java.util.ArrayList<Enemy>();
-
     /**
-     * Constructs list of enemies via "Enemies.txt"
+     * This method will generate an enemy from the list with its level
+     * 
+     * @return - Randomly generated Enemy
      */
-    public EnemyGenerator() {
-        String next;
-    	File file = new File( "Enemies.txt" );
-        try {
-		    Scanner r = new Scanner( file );
-
-            while( r.hasNextLine() ) {
-                next = r.nextLine();
-                String[] earr = next.split( "," );
-                enemyList.add( new Enemy( earr[0] , Integer.parseInt( earr[1] ) ) );
+    public Enemy generateEnemy(int level) {
+        Enemy outEnemy;
+        int rand_enemy = (int)(Math.random() * 4);
+        switch( rand_enemy ){
+            case 0:
+                outEnemy = new Troll();
+                break;
+            case 1:
+                outEnemy = new Orc();
+                break;
+            case 2:
+                outEnemy = new Froglok();
+                break;
+            case 3:
+                outEnemy = new Goblin();
+        }
+        if( level > 1 ){
+            rand_enemy = (int)(Math.random() * 2);
+            
+            for( int i = 1; i < level; ++i ){
+                switch( rand_enemy ){
+                    case 0:
+                        outEnemy = new Warrior(outEnemy);
+                    case 1:
+                        outEnemy = new Warlock(outEnemy);
+                }
             }
-            r.close();
-        } catch (java.io.FileNotFoundException e){
-            System.out.println("FileNotFoundException");
-            e.printStackTrace();
         }
-
-    }
-
-      /**
-       * This method will generate an enemy from the list with its level
-       * 
-       * @return - Randomly generated Enemy
-       */
-
-    public Enemy generateEnemy() {
-        int mNum = (int)( Math.random() * enemyList.size() );
-        boolean isMagic = ( ( (int)( Math.random() * 2 ) ) == 1 );
-        if( isMagic ) {
-            return new MagicalEnemy( enemyList.get( mNum ).getName() + " Warlock" , enemyList.get( mNum ).getHP() );
-        } else {
-            return new Enemy( enemyList.get( mNum ).getName(), enemyList.get( mNum ).getMaxHP() );
-        }
+        return outEnemy;
+        
     }
 }
