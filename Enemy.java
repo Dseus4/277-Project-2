b2ac1cbd79becff9463e590e52564d774c09ee84
Dseus4 <tsumakig@gmail.com>
2021-04-25 00:27:53 -0700

    removed attack()

--- Enemy.java
+++ Enemy.java
@@ -2,7 +2,7 @@
 /**
  * This class will create an enemy that the Hero will fight in the game.
  */
-class Enemy extends Entity {
+abstract class Enemy extends Entity {
     /**
      * Constructs Enemy with name and max HP.
      * 
@@ -11,17 +11,5 @@ class Enemy extends Entity {
      */
     public Enemy( String n, int mHp ){
         super( n, mHp );
-    }
-
-    /**
-     * Physical attack by Enemy against entity (1 - 2*maxHP).
-     * 
-     * @param e - Entity the enemy is attacking
-     * @return - String representation of attack
-     */
-    public String attack( Entity e ){
-        int d = (int)( Math.random() * ( ( getMaxHP() * 2 ) - 1 ) ) + 1;
-        e.takeDamage( d );
-        return getName() + " attacks " + e.getName() + " for " + d + " damage.";
     }
 }
