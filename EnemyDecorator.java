ec9c64729105fc724cbf9d99db35ee24810380fd
MichaelGrendahl <82973090+MichaelGrendahl@users.noreply.github.com>
2021-04-28 12:35:54 -0700

    Update EnemyDecorator.java
    Changed Constructor

--- EnemyDecorator.java
+++ EnemyDecorator.java
@@ -1,8 +1,8 @@
 public abstract class EnemyDecorator extends Enemy{
 
     protected Enemy enemy;
-    public EnemyDecorator( Enemy e ) {
-        super( e.getName() , e.getHP() );
+    public EnemyDecorator( Enemy e, String n, int h) {
+        super( n + e.getName(), e.getMaxHP() + h );
         enemy = e;
     }
 
